003caf48d0d5ed6c651f83b7f4a0d00ee7de0e2b
JLMAC30 <116308725+JLMAC30@users.noreply.github.com>
2022-11-26 19:07:49 -0700

    Add files via upload

---
+++ loan_analyzer.py
@@ -0,0 +1,28 @@
+loan_costs = [500, 600, 200, 1000, 450]
+print(loan_costs)
+number_of_loans = len(loan_costs)
+print(f"There are {number_of_loans} loans.")
+total_of_loans = sum(loan_costs)
+print(f"The sum of all the loans is {total_of_loans}.")
+average_loan_price = total_of_loans / number_of_loans
+print(f"The average loan price is {average_loan_price}.")
+future_value = 1500
+print(future_value)
+remaining_months = 5
+print(remaining_months)
+present_value = 1000
+print(present_value)
+discount_rate = 20
+print(discount_rate)
+present_value = future_value / (1+discount_rate)
+annual_discount_rate = 0.20
+
+    
+
+
+
+
+
+
+
+
